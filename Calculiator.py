e812235dfe601d28faa4b1e114c62b4b56ba657b
Lev Zirnis <115003550+leofcfvzr@users.noreply.github.com>
2023-10-07 20:48:23 +0300

    Add files via upload

---
+++ Calculiator.py
@@ -0,0 +1,35 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def add(x, y):
+...     return x + y
+... 
+... def subtract(x, y):
+...     return x - y
+... 
+... def multiply(x, y):
+...     return x * y
+... 
+... def divide(x, y):
+...     return x / y
+... 
+... print("Выберите операцию:")
+... print("1. Сложение")
+... print("2. Вычитание")
+... print("3. Умножение")
+... print("4. Деление")
+... 
+... choice = input("Введите номер операции (1/2/3/4): ")
+... 
+... num1 = float(input("Введите первое число: "))
+... num2 = float(input("Введите второе число: "))
+... 
+... if choice == '1':
+...     print(num1, "+", num2, "=", add(num1, num2))
+... elif choice == '2':
+...     print(num1, "-", num2, "=", subtract(num1, num2))
+... elif choice == '3':
+...     print(num1, "*", num2, "=", multiply(num1, num2))
+... elif choice == '4':
+...     print(num1, "/", num2, "=", divide(num1, num2))
+... else:
+...     print("Неверный ввод")
